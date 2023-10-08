343bb3dd759b416f9afe9f54a5ab2593bd91c2d8
hamayou-61 <caster2596@gmail.com>
2023-10-08 16:34:53 +0900

    fix line breaks

--- src/helpers/calculateWinner.tsx
+++ src/helpers/calculateWinner.tsx
@@ -16,36 +16,16 @@ export default function calculateWinner(squares: Array<string | null>) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c, d, e] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
+    if (
+      squares[a] && 
+      squares[a] === squares[b] && 
+      squares[a] === squares[c] && 
+      squares[a] === squares[d] && 
+      squares[a] === squares[e]
+    ) {
       return squares[a];
     }
   }
 
   return null;
 }
-
-
-// export default function calculateWinner(history: Array<string | null>){
-//   const lines = [
-//     [0,1,2,3,4],
-//     [0,18,36,54,72],
-//     [0,19,38,57,76],
-//     [4,21,38,55,72]
-//   ];
-//   for(let i=0; i<lines.length; i++){
-//     const [a,b,c,d,e] = lines[i];
-//     for(let j=0; j<14; j++){ // 檢查橫向是否 win
-//       for(let k=0; k<21; k++){ // 檢查縱向是否 win
-//         if( history[a+j+(k*18)]
-//           && history[a+j+(k*18)] === history[b+j+(k*18)]
-//           && history[b+j+(k*18)] === history[c+j+(k*18)]
-//           && history[c+j+(k*18)] === history[d+j+(k*18)]
-//           && history[d+j+(k*18)] === history[e+j+(k*18)]
-//           ){
-//           return history[a+j+(k*18)];
-//         }
-//       }
-//     }
-//   }
-//   return false;
-// }
